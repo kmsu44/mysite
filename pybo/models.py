e1177b67dc9a1f3003db67b4e8f50baf53d0ed96
kmsu44 <45655623+kmsu44@users.noreply.github.com>
2022-05-20 19:28:57 +0900

    Add files via upload

--- pybo/models.py
+++ pybo/models.py
@@ -1,22 +1,10 @@
 from django.db import models
 # from django.contrib.auth.models import AbstractUser
 # user객체 확장
-class User():
-    FirstName = models.CharField()
-    phone_number = models.CharField(max_length=11)
-    address = models.CharField()
-
-class Question(models.Model):
-    subject = models.CharField(max_length=200)
-    content = models.TextField()
-    create_date = models.DateTimeField()
-
-    def __str__(self):
-        return self.subject
-class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    content = models.TextField()
-    create_date = models.DateTimeField()
+# class User():
+#     FirstName = models.CharField()
+#     phone_number = models.CharField(max_length=11)
+#     address = models.CharField()
 
 # 상품 모델
 class Post(models.Model):
