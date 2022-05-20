fe8944c24670df25c5f8e6a93e28af02d2f4782d
kmsu44 <45655623+kmsu44@users.noreply.github.com>
2022-05-20 19:27:08 +0900

    Delete projects/mysite directory

--- projects/mysite/pybo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-# app_name = 'pybo'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:Post_id>/', views.detail, name='detail'),
-]
-
